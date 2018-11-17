6ba942823d084c3724ad16b53a4299b628e24e0e
Shiraj Ganguly <shirajg@protonmail.com>
2018-11-17 10:49:41 -0500

    Set up the creation form to submit to the API

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,10 +1,28 @@
 import Controller from '@ember/controller';
+import $ from 'jquery';
 
 export default Controller.extend({
+  errors: undefined,
+  shortURL: undefined,
   fullURL: '',
   actions: {
     createURL() {
-      this.set('fullURL','');
+      this.set('errors', undefined);
+      $.ajax({
+        url: 'http://localhost:3000',
+        method: 'POST',
+        data: {
+          url: this.get('fullURL')
+        },
+        success: (data) => {
+          this.set('fullURL', '');
+          this.set('errors', undefined);
+          this.set('shortURL', data.shortened_link)
+        },
+        error: (jqXHR, textStatus) => {
+          this.set('errors', textStatus)
+        }
+      })
     }
   }
 });
